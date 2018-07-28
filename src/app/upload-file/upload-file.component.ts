4161dcd999bf2b8fcb8227568ad8a2d913c7ec28
Santiago de la Fuente <santiago.daniel.delafuente@hotmail.com>
2018-07-28 18:18:10 -0300

    Post Final

--- src/app/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Http, Headers,Response,RequestOptions } from '@angular/http';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
 import { UploadEvent, UploadFile, FileSystemFileEntry, FileSystemDirectoryEntry } from 'ngx-file-drop';
 import { Persona } from '../clases/persona';
 
@@ -10,9 +10,9 @@ import { Persona } from '../clases/persona';
 })
 export class UploadFileComponent implements OnInit {
 
-    miPersona = new Persona("","","","");
+    miPersona = new Persona('', '', '', '');
     public files: UploadFile[] = [];
-    public archivos : Array<any>;
+    public archivos: Array<any>;
 
     constructor( public http: Http ) { }
 
@@ -29,20 +29,23 @@ export class UploadFileComponent implements OnInit {
                 const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
 
                 fileEntry.file((file: File) => {
-                // You could upload it like this:
-                const formData = new FormData()
-                formData.append('nombre', this.miPersona.nombre );
-                formData.append('email', this.miPersona.email );
-                formData.append('password', this.miPersona.password );
-                formData.append('sexo', this.miPersona.sexo );
-                formData.append('foto', file, droppedFile.relativePath);
 
-                console.log( JSON.stringify(this.miPersona) );
-                /** const headers = new HttpHeaders({'security-token': 'mytoken'}) */
-                this.http.post('http://localhost:8080/archivo/', formData).subscribe(data => {
-                    console.log(data);
-                })
-              });
+                    // You could upload it like this:
+                    const formData = new FormData();
+                    formData.append('nombre', this.miPersona.nombre );
+                    formData.append('email', this.miPersona.email );
+                    formData.append('password', this.miPersona.password );
+                    formData.append('sexo', this.miPersona.sexo );
+                    formData.append('foto', file, droppedFile.relativePath);
+
+                    // console.log( JSON.stringify(this.miPersona) );
+                    /** const headers = new HttpHeaders({'security-token': 'mytoken'}) */
+                    // {email:mail@mail.com,password:12345678a}
+                    this.http.post('http://localhost:8080/archivo/', formData).subscribe(data => {
+                        console.log(data);
+                    });
+
+                });
             } else {
               // It was a directory (empty directories are added, otherwise only files)
               const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
@@ -51,22 +54,19 @@ export class UploadFileComponent implements OnInit {
         }
     }
 
-    public fileOver(event){
-      //console.log(event);
+    public fileOver(event) {
+      // console.log(event);
     }
 
-    public fileLeave(event){
-      //console.log(event);
+    public fileLeave(event) {
+      // console.log(event);
     }
 
-    private verificarPersona()
-    {
-        if(this.miPersona.nombre == "" || this.miPersona.email == "" || this.miPersona.password == ""){
-            console.log("Falta cargar datos ");
+    private verificarPersona() {
+        if (this.miPersona.nombre === '' || this.miPersona.email === '' || this.miPersona.password === '') {
+            console.log('Falta cargar datos ');
             return  false;
         }
         return true;
     }
-
-
 }
